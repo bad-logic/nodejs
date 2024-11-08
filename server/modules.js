8485c5e1ffcdc4e2a233d81af5e81a6afd4e44db
roshan <45297269+bad-logic@users.noreply.github.com>
2024-11-08 14:14:42 -0600

    doc: cleanup

--- server/modules.js
+++
@@ -1,9 +0,0 @@
-// exports.express = require("express");
-// exports.favicon = require("static-favicon");
-
-const mods = {};
-mods.express = require("express");
-mods.favicon = require("static-favicon");
-mods.faviconserve = require("serve-favicon");
-
-module.exports = Object.freeze(mods);
